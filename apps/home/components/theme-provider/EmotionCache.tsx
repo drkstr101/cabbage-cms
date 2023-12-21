4e20fdf0fe3a5dd4d4f3f7185381108bfb1cd832
Aaron Miller <amiller@aps.org>
2023-12-21 23:33:13 +0000

    chore: run prettier

--- apps/home/components/theme-provider/EmotionCache.tsx
+++ apps/home/components/theme-provider/EmotionCache.tsx
@@ -17,7 +17,9 @@ export type NextAppDirEmotionCacheProviderProps = {
 };
 
 // Adapted from https://github.com/garronej/tss-react/blob/main/src/next/appDir.tsx
-export default function NextAppDirEmotionCacheProvider(props: NextAppDirEmotionCacheProviderProps) {
+export default function NextAppDirEmotionCacheProvider(
+  props: NextAppDirEmotionCacheProviderProps
+) {
   const { options, CacheProvider = DefaultCacheProvider, children } = props;
 
   const [registry] = React.useState(() => {
