dadfcd38df5fb764405424bf1418b9b3512453f4
Aaron Miller <amiller@aps.org>
2023-12-28 21:13:05 +0000

    feat: add tailwindcss support

---
+++ tailwind-workspace-presets.js
@@ -0,0 +1,26 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: ({ colors }) => ({
+        neutral: colors.neutral,
+        primary: colors.cyan,
+        secondary: colors.sky,
+        accent: colors.teal,
+        danger: colors.rose,
+        warning: colors.amber,
+        success: colors.emerald,
+        white: '#fafafa',
+        black: '#0a0a0a',
+      }),
+      fontFamily: {
+        book: [...fontFamily.sans],
+        display: [...fontFamily.sans],
+        code: [...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+};
